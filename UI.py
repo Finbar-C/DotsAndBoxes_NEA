d6bd672e5c86bb92bca8ebe3370442ae7ff54d95
TABLET-7JR9V15O\finba <fhachambers@gmail.com>
2022-06-22 15:44:42 +0100

    Provides the ability to run something

--- UI.py
+++ UI.py
@@ -1,12 +1,29 @@
 from game import *
 from accounts import *
+from tkinter import *
+from itertools import product
+from abc import ABC, abstractmethod
+
+class UI():
+
+    @abstractmethod
+    def run(self):
+        raise NotImplementedError
 
 class Terminal():
     def __init__(self):
         self.__game = Game()
+    
+    def run(self):
+        pass
 
 class GUI():
-    pass
+    def __init__(self):
+        root = Tk()
+        root.title("Dots and Boxes")
+    
+    def run(self):
+        self.__root.mainloop()
 
 class NewGame(GUI):
     pass # displays new game creation window
--- main.py
+++ main.py
@@ -1,8 +1,22 @@
 from UI import *
+from sys import argv
 
-runWith = None
+def usage():
+    print(f"""
+Usage: {argv[0]} [g | t]
+g: play with GUI
+t: play with Terminal""")
+    quit()
+
+
 if __name__ == "__main__":
-    if runWith == "t":
+    if len(argv) != 2:
+        usage()
+    elif argv[1] == "t":
         game = Terminal()
-    elif runWith == "g":
+    elif argv[1] == "g":
         game = GUI()
+    else:
+        usage()
+    
+    game.run()
