6f249283edb7ef2e945b53f49837f1f120dff90f
TABLET-7JR9V15O\finba <fhachambers@gmail.com>
2022-06-29 11:51:30 +0100

    Added ability to update board

--- game.py
+++ game.py
@@ -1,25 +1,11 @@
 from UI import *
 
-class Game():
-
-    def __init__(self, dims: tuple, pnum: int, names: tuple):
-        self.__board = Board(dims[0], dims[1])
-        players = []
-        for i in range(pnum):
-            x = Player(names[i])
-            players.append(x)
-
-    def place(self, pos, dir):
-        pass
-
-    def __repr__(self):
-        return str(self.__board)
 
 class Board():
 
     EMPTY = "  "
-    LINE_H = "--"
-    LINE_V = "|"
+    H_LINE = "--"
+    V_LINE = "|"
 
     def __init__(self, rows: int, cols: int):
         self.__grid = [[(Board.EMPTY, Board.EMPTY, Board.EMPTY, Board.EMPTY) for _ in range(cols)] for _ in range(rows)]
@@ -31,13 +17,82 @@ class Board():
             for boxy in range(self.__cols):
                 pass
             #go box by box, check each side
+    
+    def checkBox(self, pos: tuple):
+        for i in range(4):
+            if (self.__grid[pos[0]][pos[1]])[i] == Board.EMPTY:
+                return False
+        return True
+    
+    def directionConvert(self, dir:str):
+        if dir.upper() == "N":
+            return 0
+        elif dir.upper() == "S":
+            return 1
+        elif dir.upper() == "E":
+            return 2
+        elif dir.upper() == "W":
+            return 3
+    
+    def checkClear(self, pos: tuple, dir: str):
+        dir = Board.directionConvert(dir)
+        if (self.__grid[pos[0]][pos[1]])[dir] == Board.EMPTY:
+            return True
+        return False
+    
+    def place(self, pos: tuple, dir: str):
+        dir = Board.directionConvert(dir)
+        if dir <= 1:
+            (self.__grid[pos[0]][pos[1]])[dir] = Board.H_LINE
+        elif dir >= 2:
+            (self.__grid[pos[0]][pos[1]])[dir] = Board.V_LINE
+
+
+
             
             
 
 class Player():
-    def __init__(self, name):
+    def __init__(self, name: str):
         self.__name = name
+        self.__claimedBoxes = 0
+    
+    def addBox(self):
+        self.__claimedBoxes += 1
+    
+    @property
+    def getName(self):
+        return self.__name
 
 if __name__ == "__main__":
     game = Terminal()
     game.run()
+
+class Game():
+
+    def __init__(self, dims: tuple, pnum: int, names: tuple):
+        self.__board = Board(dims[0], dims[1])
+        self.players = []
+        for i in range(pnum):
+            x = Player(names[i])
+            self.players.append(x)
+        self.__turn = 0
+    
+    @property
+    def getTurn(self):
+        return self.__turn
+
+    def place(self, pos: tuple, dir: str):
+        if self.__board.checkClear(pos, dir):
+            self.__board.place(pos, dir)
+        
+
+    def __repr__(self):
+        return str(self.__board)
+    
+    def End(self):
+        pass
+
+    def nextTurn(self):
+        pass
+
