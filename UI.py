1bc3998e3e73c6bf3eee86b20bb117a2861c2714
TABLET-7JR9V15O\finba <fhachambers@gmail.com>
2022-09-21 17:51:46 +0100

    Began GUI

--- UI.py
+++ UI.py
@@ -46,18 +46,54 @@ class GUI(UI):
     def __init__(self):
         self.__root = Tk()
         self.__root.title("Dots and Boxes")
+        frame = Frame(self.__root)
+        frame.pack()
+        Button(frame, text="Help", command=self.__showHelpMain).pack(fill=X)
+        Button(frame, text="New Game", command=self.__NewGame).pack(fill=X)
+        Button(frame, text="Options", command=self.__Settings).pack(fill=X)
+        Button(frame, text="Exit", command=self.__Exit).pack(fill=X)
+
+        scroll = Scrollbar(frame)
+        console = Text(frame, height=4, width = 50)
+        scroll.pack(side=RIGHT, fill = Y)
+        console.pack(side=LEFT, fill = Y)
+        scroll.config(command=console.yview)
+        console.config(yscrollcommand=scroll.set)
+        self.__console = console
+
+
     
     def run(self):
         self.__root.mainloop()
 
-class NewGame(GUI):
-    pass # displays new game creation window
+    def __NewGame(self):
+        window = Tk()
+        window.title("Game Creation Menu")
+        frame = Grid(window)
 
-class Settings(GUI):
-    pass # displays settings window
+    def __Settings(self):
+        pass # displays settings window
 
-class GameWin(GUI):
-    pass # window in which game is played
+    def __GameWin(self):
+        pass # window in which game is played
+
+    def __showHelpMain(self):
+        window = Tk()
+        window.title("Dots and Boxes - Help")
+        window = Toplevel(self.__root)
+        frame = Frame(window)
+        frame.pack()
+        Label(frame, text="Filler Text Until I Figure Out What To Write in The Help Window")
+
+        scroll = Scrollbar(frame)
+        console = Text(frame, height=4, width = 50)
+        scroll.pack(side=RIGHT, fill = Y)
+        console.pack(side=LEFT, fill = Y)
+        scroll.config(command=console.yview)
+        console.config(yscrollcommand=scroll.set)
+
+    def __Exit(self):
+        self.__root.quit()
 
 
 if __name__ == "__main__":
